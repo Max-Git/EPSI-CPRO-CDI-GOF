ebcb40e9b3eb14311424449247b701c42a785e57
Max <max_address@yahoo.com>
2018-02-11 15:42:39 +0100

    Initial commit

---
+++ Strategy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pas de strategy :( !");
+
+            bool isHttps = false;
+
+            Client c = new Client();
+            
+            if (isHttps)
+                c.ProcessHttpsRequest();
+            else
+                c.ProcessHttpRequest();
+        }
+    }
+}
+
+class Client 
+{
+
+
+    public void ProcessHttpsRequest()
+    {
+        Console.WriteLine("TraitementSecurisé...");
+    }
+
+    public void ProcessHttpRequest()
+    {
+        Console.WriteLine("Traitement pas Securisé...");
+    }
+}
