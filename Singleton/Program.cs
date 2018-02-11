ea3f90a4c1f7b93874ec94d321fc8dec5c36332e
Max <max_address@yahoo.com>
2018-02-11 16:02:15 +0100

    Add Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test singleton");
+            Singleton.GetInstance.DisplayHello();
+            Singleton.GetInstance.DisplayHello();
+            Singleton.GetInstance.DisplayHello();
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        private Singleton() { }
+        
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Console.WriteLine("Nouvelle instance");
+                    instance = new Singleton();
+                }
+                else
+                {
+                    Console.WriteLine("Même instance");
+                }
+
+                return instance;
+            }
+        }
+
+        public void DisplayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+         
+    }
+}
